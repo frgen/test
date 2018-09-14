c155ff9610756257df3584354e2e0e952b57ac47
frgen <31874180+frgen@users.noreply.github.com>
2018-09-14 00:07:06 -0300

    Add files via upload

---
+++ Clase7(linux)/Info1.h
@@ -0,0 +1,3 @@
+float calcularPromedio(int, int, int);
+int cargarAlumnos(char[][50], char[][50], char[], int[], int[], int[], float[], int[], int);
+int mostrarAlumnos(char[][50], char[][50], char[], int[], int[], int[], float[], int[], int);
